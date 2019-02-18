1343402876a01199fa49e87462b56428398ad518
Md Nizam Uddin Chy <Nizam@Mds-MacBook-Pro.local>
2019-02-17 23:38:00 -0700

    Added model.prediction() for predicting a test image. Also reduced the number of neurons in the 1st hidden layer

--- Image_classification_mnist.py
+++ Image_classification_mnist.py
@@ -3,6 +3,7 @@ Tensorflow tutorial for image classification in MNIST dataset
 '''
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
+import numpy as np
 
 #Only needed to plot an image
 import matplotlib.pyplot as plt
@@ -15,7 +16,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 #Creating model
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape =(28,28)),
-    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dense(256, activation=tf.nn.relu),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
@@ -35,3 +36,8 @@ model.fit(x_train, y_train, epochs=5)
 
 #Testing/evaluating the model
 model.evaluate(x_test, y_test)
+
+#Making prediction with the model
+predictions = model.predict(x_test)
+print("Prediction for x_test[0]: ")
+print(np.argmax(predictions[0]))
